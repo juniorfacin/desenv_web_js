969cddffba6caa5d8b1046540cf0eef15e9511be
Junior Facin <jufacin@hotmail.com>
2024-06-01 12:35:47 -0300

    Manipulando elementos no DOM: 'Fase 4 - Desenvolvendo o temporizador'

--- fokus-projeto-base/script.js
+++ fokus-projeto-base/script.js
@@ -4,21 +4,47 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
 
-// Refatorando o código com funções.
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('./sons/play.wav');
+const audioPause = new Audio('./sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
+
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
@@ -43,3 +69,33 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play();
+        zerar()
+        alert('Tempo encerrado!');
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPause.play();
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+
+
